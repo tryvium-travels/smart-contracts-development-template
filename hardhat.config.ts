8ba2b0f35f9a712a9dbeaab3cc23b7e00110a579
Alessandro Sanino <alessandro@sanino.dev>
2021-12-27 22:58:29 +0100

    Updated and fixed template
    Signed-off-by: Alessandro Sanino <alessandro@sanino.dev>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,18 +2,18 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-import '@nomiclabs/hardhat-waffle'
-import '@nomiclabs/hardhat-ethers'
-import '@typechain/hardhat'
-import "@atixlabs/hardhat-time-n-mine"
-import "hardhat-gas-reporter"
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "@atixlabs/hardhat-time-n-mine";
+import "hardhat-gas-reporter";
 
 module.exports = {
-  solidity: "0.8.6",
+  solidity: "0.8.11",
   paths: {
-    sources: "./src",
+    sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./artifacts"
-  }
+    artifacts: "./artifacts",
+  },
 };
